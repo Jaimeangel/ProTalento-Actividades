3cc45cb752e9b229d759b75d039d0a587e8655c8
Jaime A <jaimeangel1097@gmail.com>
2023-11-15 22:38:50 -0500

    segunda parte proyecto integrador

--- proyecto-fundamentos-de-python/main.py
+++ proyecto-fundamentos-de-python/main.py
@@ -1,3 +1,16 @@
-jugador=input('Escribe tu nombre jugador: ')
+from readchar import readkey, key
 
+jugador=input('Escribe tu nombre jugador: ')
 print(f"Hola {jugador} bienvenido a esta aventura")
+print(f"A continuacion podras jugar con tu teclado presionando cualquier tecla y podras visualizarla en pantalla")
+print(f"Si quiere parar el juego presiona la tecla ↑")
+
+while True:
+  k = readkey()
+  if k == key.UP:
+    break
+  print(k)
+  
+print(f"¡Gracias {jugador}, presionaste la tecla ↑, fin del juego! ")
+
+
