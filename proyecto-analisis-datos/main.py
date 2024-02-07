2fe7a25cddf84363de4f4a8e70cd39e9bb67e9aa
Jaime A <jaimeangel1097@gmail.com>
2024-02-07 09:24:53 -0500

    proyecto parte 3: calculando analiticas simples

--- proyecto-analisis-datos/main.py
+++ proyecto-analisis-datos/main.py
@@ -13,3 +13,13 @@ df_sobrevivientes = df[df['is_dead'] == 0]
 
 promedio_perecidos = df_perecidos.groupby('is_dead')['age'].mean()
 promedio_sobrevivientes = df_sobrevivientes.groupby('is_dead')['age'].mean()
+
+# tipos de datos SI son correctos en cada colúmna
+comprobacion_tipos_dato = df.dtypes
+print(comprobacion_tipos_dato)
+
+# Calcular la cantidad de hombres fumadores vs mujeres fumadoras
+filtro_smokers = df['is_smoker'] == True
+nuevo_df = df[filtro_smokers]
+agrupar_sex_smoker = nuevo_df.groupby(['is_male', 'is_smoker']).size()
+print(agrupar_sex_smoker)
