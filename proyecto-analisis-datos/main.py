2e8429903ce9305eb2edefc1e922572945499b69
Jaime A <jaimeangel1097@gmail.com>
2024-02-07 08:47:40 -0500

    proyecto integrador: carga de datos

--- proyecto-analisis-datos/main.py
+++ proyecto-analisis-datos/main.py
@@ -1,11 +1,15 @@
 from datasets import load_dataset
 import numpy as np
+import pandas as pd
 
 dataset = load_dataset("mstz/heart_failure")
 
 data = dataset["train"]
 
-edades_paciente = np.array(data['age'])
-promedio_edades = np.mean(edades_paciente)
+df = pd.DataFrame(data)
 
-print(promedio_edades)
+df_perecidos = df[df['is_dead'] == 1]
+df_sobrevivientes = df[df['is_dead'] == 0]
+
+promedio_perecidos = df_perecidos.groupby('is_dead')['age'].mean()
+promedio_sobrevivientes = df_sobrevivientes.groupby('is_dead')['age'].mean()
