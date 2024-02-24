1b71df4af6b7143bdb128836008c42f6710a3235
Jaime A <jaimeangel1097@gmail.com>
2024-02-23 21:19:21 -0500

    Parte 5: Limpieza y preparación de datos

--- proyecto-analisis-datos/main.py
+++ proyecto-analisis-datos/main.py
@@ -18,3 +18,44 @@ def urlToCSV(URL):
         print('Error request API')
 
 urlToCSV(URL)
+
+csv_api = pd.read_csv('api_data.csv')
+
+def calcular_rango_intercuartil(columna,dataframe):
+     # Calcular el rango intercuartil (IQR)
+    Q1_cantidad = dataframe[columna].quantile(0.25)
+    Q3_cantidad = dataframe[columna].quantile(0.75)
+    IQR_cantidad = Q3_cantidad - Q1_cantidad
+
+    # Definir los límites para detectar valores atípicos
+    lower_bound_cantidad = Q1_cantidad - 1.5 * IQR_cantidad
+    upper_bound_cantidad = Q3_cantidad + 1.5 * IQR_cantidad
+
+    # Filtrar los valores atípicos
+    dataframe=dataframe[(dataframe[columna] >= lower_bound_cantidad) & (dataframe[columna] <= upper_bound_cantidad)]
+    return dataframe
+
+def analyze_data(dataframe):
+    # Eliminamos duplicados
+    df = dataframe.drop_duplicates(keep='first')
+    # Verificar valores faltantes
+    numero_valores_faltantes = df.isna().sum()
+    # No hay ningun valor faltante por columna
+
+    nombre_columnas = df.columns.values.tolist()
+    # filtramos el dataFrame,removiendo valores atípicos
+    for columna in nombre_columnas:
+        # por cada columna filtramos por valores atipicos segun el rango intercuartil
+        dataframe = calcular_rango_intercuartil(columna,dataframe)
+
+    # 
+    rangos = [-float("inf"), 12, 19, 39, 59, float("inf")]
+    etiquetas = ['Niño', 'Adolescente', 'Jóvenes adulto', 'Adulto', 'Adulto mayor']
+
+    dataframe['age group'] = pd.cut(df['age'], bins=rangos, labels=etiquetas, right=False)
+
+    dataframe.to_csv('analyze_data.csv', index=False)
+
+
+analyze_data(csv_api)
+
